26e8c43dea88feb14dceefa0c723c3168fa6eb87
본ìjwehan < jwehan@naver.com>
2023-04-05 13:44:48 +0900

    1Week?

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -0,0 +1,78 @@
+package com.ll.gramgram.boundedContext.likeablePerson.entity;
+
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@Entity
+@Getter
+public class LikeablePerson {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+    @ManyToOne
+    private InstaMember fromInstaMember; // 호감을 표시한 사람(인스타 멤버)
+    private String fromInstaMemberUsername; // 혹시 몰라서 기록
+    @ManyToOne
+    private InstaMember toInstaMember; // 호감을 받은 사람(인스타 멤버)
+    private String toInstaMemberUsername; // 혹시 몰라서 기록
+    private int attractiveTypeCode; // 매력포인트(1=외모, 2=성격, 3=능력)
+
+    public InstaMember getFromInstaMember() {
+        return fromInstaMember;
+    }
+
+    public void setFromInstaMember(InstaMember fromInstaMember) {
+        this.fromInstaMember = fromInstaMember;
+    }
+
+    public String getFromInstaMemberUsername() {
+        return fromInstaMemberUsername;
+    }
+
+    public void setFromInstaMemberUsername(String fromInstaMemberUsername) {
+        this.fromInstaMemberUsername = fromInstaMemberUsername;
+    }
+
+    public InstaMember getToInstaMember() {
+        return toInstaMember;
+    }
+
+    public void setToInstaMember(InstaMember toInstaMember) {
+        this.toInstaMember = toInstaMember;
+    }
+
+    public String getToInstaMemberUsername() {
+        return toInstaMemberUsername;
+    }
+
+    public void setToInstaMemberUsername(String toInstaMemberUsername) {
+        this.toInstaMemberUsername = toInstaMemberUsername;
+    }
+
+    public String getAttractiveTypeDisplayName() {
+        return switch (attractiveTypeCode) {
+            case 1 -> "외모";
+            case 2 -> "성격";
+            default -> "능력";
+        };
+
+    }
+}
